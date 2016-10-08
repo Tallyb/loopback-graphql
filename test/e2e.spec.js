cf8e26b36b8d1ada8218cb27a2b5f97dde279825
tallyb <tally.barak@gmail.com>
2016-10-08 21:23:23 +0300

    mutation test

--- test/e2e.spec.js
+++ test/e2e.spec.js
@@ -39,4 +39,32 @@ describe('query', () => {
             });
     });
 
+    it('should add an author', () => {
+        const author = gql`
+            mutation save ($obj: AuthorInput!) {
+  saveAuthor (obj: $obj) {
+	first_name
+    last_name
+    birth_date
+        }
+    }
+
+        `;
+        const values = {
+            obj:
+                {
+                    first_name:'Virginia',
+                    last_name: 'Wolf',
+                    birth_date: new Date()
+                }
+        };
+
+        return chai.request('localhost:3000')
+            .post('/graphql')
+            .send({query: author, variables:values})
+            .then(res => {
+                expect(res).to.have.status(200);
+                console.log('RES', res.body);
+            }, err => console.log(err));
+    });
 });
