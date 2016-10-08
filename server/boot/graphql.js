f01e92774fd00ccc67f943bdc3aedc728691eb95
tallyb <tally.barak@gmail.com>
2016-10-08 11:02:12 +0300

    chore(test) initial test infrastructure and linting

--- server/boot/graphql.js
+++ server/boot/graphql.js
@@ -1,5 +1,5 @@
 'use strict';
 var loopbackGraphql = require('../../index.js');
 module.exports = function(app) {
-  loopbackGraphql.boot(app);
+    loopbackGraphql.boot(app);
 };
---
+++ test/query.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+var expect = require('chai').expect;
+// var testHelper = require('./testHelper');
+// var _ = require('lodash');
+
+describe('query', ()=>{
+    it('should return true', ()=>{
+        expect(true).to.be.true;
+    });
+
+});
