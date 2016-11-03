8d5c8a056486e319bc36ab25a5b5ae5550d4cd89
tallyb <tally.barak@gmail.com>
2016-11-03 18:49:14 +0200

    feat(app): Fix hidden attributes

--- common/models/author.js
+++ common/models/author.js
@@ -1,34 +1,30 @@
 'use strict';
 
-module.exports = function (Author) {
+module.exports = function(Author) {
 
     Author.remoteMethod(
         'addFriend', {
             'http': {
                 'path': '/addFriend',
                 'verb': 'post'
-
             },
 
-            'accepts': [
-                {
-                    'arg': 'author',
-                    'type': 'number'
-                },{
-                    'arg': 'friend',
-                    'type': ['number']
-                }
-            ],
+            'accepts': [{
+                'arg': 'author',
+                'type': 'number'
+            }, {
+                'arg': 'friend',
+                'type': ['number']
+            }],
 
             'returns': {
                 'arg': 'result',
                 'type': 'object'
-
             }
         }
-);
+    );
 
-    Author.addFriend = function (author, friend) {
+    Author.addFriend = function(author, friend) {
 
         return Author.findById(author)
             .then(res => {
--- common/models/note.js
+++ common/models/note.js
@@ -1,4 +1,28 @@
 'use strict';
 
 module.exports = function(Note) {
+
+    function clear() {
+        return {
+            note: {
+                Content: ''
+            },
+            previousClear: new Date()
+        };
+    }
+
+    Note.remoteMethod(
+        'clear', {
+            'http': {
+                'path': '/clear',
+                'verb': 'post'
+            },
+            'returns': [{
+                'arg': 'note',
+                'type': 'object'
+            }, {
+                'arg': 'previousClear',
+                'type': 'Date'
+            }]
+        });
 };
