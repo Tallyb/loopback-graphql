93e8da73f678f14f6f28298abb8a413eea3c31d4
Tally Barak <tally.barak@gmail.com>
2017-01-06 11:27:56 +0200

    fix(app): Fix wrong mixins

--- test/model.spec.js
+++ test/model.spec.js
@@ -4,7 +4,6 @@ var expect = require('chai').expect;
 describe('model testing', () => {
     it.skip('should return models', (done) => {
         server.on('started', function() {
-            console.log('SERVER STARTED');
             expect(server).not.to.be.empty;
             done();
         });
